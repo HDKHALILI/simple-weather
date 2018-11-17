d5015bffd2ba17952826f328b76844f439b00e42
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2018-11-17 20:11:37 +1100

    Initial Commit

---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Form = (props) => (
+  <form>
+    <input type="text" name="location" placeholder="Sydney" />
+    <button>Get Weather</button>
+  </form>
+);
+
+export default Form;
